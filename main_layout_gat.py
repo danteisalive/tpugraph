f54f60b4b10b87826a2890f6e7f47c946379a85b
danteisalive <as3mx@virginia.edu>
2023-10-30 19:21:12 +0000

    added valid loop

--- main_layout_gat.py
+++ main_layout_gat.py
@@ -13,25 +13,40 @@ from torch_geometric.logging import init_wandb, log
 from torch_geometric.nn import GATConv, global_mean_pool, global_max_pool
 from torch_geometric.data import Data, Batch
 
-NUM_CPUS = os.cpu_count() 
+from network.multi_element_rank_loss import MultiElementRankLoss
+from network.ListMLE_loss import ListMLELoss
+from network.kendal_tau_metric import KendallTau
 
+NUM_CPUS = os.cpu_count() 
+NUM_CONFIGS = 32
 
 class GAT(torch.nn.Module):
 
     NODE_FEATS = 140
     CONFIG_FEATS = 18
 
-    def __init__(self, hidden_channels, out_channels, heads):
+    def __init__(self, 
+                 hidden_channels, 
+                 out_channels, 
+                 heads,
+                 num_configs,
+                 ):
         super().__init__()
         
+        self.num_configs = num_configs
         in_channels = self.CONFIG_FEATS + self.NODE_FEATS + 1
 
         self.conv1 = GATConv(in_channels, hidden_channels, heads, dropout=0.6)
-        self.conv2 = GATConv(hidden_channels * heads, out_channels, heads=heads, dropout=0.6)
-        
+        self.conv2 = GATConv(hidden_channels * heads, out_channels, heads=heads, dropout=0.6)        
         self.lin = nn.Linear(out_channels * heads, 1)
 
-    def forward(self, batch, ):
+        self.loss_fn = MultiElementRankLoss(margin=0.1, number_permutations=4)
+        # self.loss_fn = ListMLELoss()
+
+        self.kendall_tau = KendallTau()
+
+    def forward(self, batch,):
+
         x = batch.x
         edge_index = batch.edge_index
         x = F.dropout(x, p=0.6, training=self.training)
@@ -41,35 +56,59 @@ class GAT(torch.nn.Module):
 
         x = global_mean_pool(x, batch.batch) + global_max_pool(x, batch.batch)
 
-        x = self.lin(x)
+        pred = self.lin(x)
+        true = batch.y
 
-        return x
+        # print("Predictions: ", pred,)
+        # print("True Labels: ",true,)
+        # print("Selected Configs: ", batch.selected_config)
+
+        # calculate loss:
+        pred = pred.view(-1, self.num_configs)
+
+        if hasattr(batch, 'selected_config'):
+            selected_configs = batch.selected_config.view(-1, self.num_configs)
+            true = true.view(-1, self.num_configs)
+            # print(pred.shape, true.shape)
+            loss = self.loss_fn(pred, true, selected_configs)
+            outputs = {'pred': pred, 'target': true, 'loss': loss}
+
+        else:
+            outputs = {'pred': pred}
+
+        return outputs
 
 
 def train(batch, model, optimizer, ):
 
     model.train()
     optimizer.zero_grad()
-    print("Train In:" , batch)
-    out = model(batch)
-    print("Train Out:" , out.shape)
-    assert(0)
+    
+    # print("Train In:" , batch)
+    outputs = model(batch)
+    # print("Train Out:" , outputs['pred'].shape, outputs['target'].shape)
 
-    # loss = F.cross_entropy(out[data.train_mask], data.y[data.train_mask])
-    # loss.backward()
-    # optimizer.step()
-    # return float(loss)
+    loss = outputs['loss']
+    loss.backward()
+    optimizer.step()
+
+    return float(loss)
 
 
 @torch.no_grad()
-def test(data, model, ):
+def validation(batch, model, ):
+    
     model.eval()
-    pred = model(data.x, data.edge_index).argmax(dim=-1)
+    
+    outputs = model(batch)
 
-    accs = []
-    for mask in [data.train_mask, data.val_mask, data.test_mask]:
-        accs.append(int((pred[mask] == data.y[mask]).sum()) / int(mask.sum()))
-    return accs
+    val_loss = outputs['loss']
+
+    model.kendall_tau.update(outputs['pred'], outputs['target'],)
+    
+    val_acc = model.kendall_tau.compute()
+    
+    return val_acc, val_loss
 
 
 if __name__ == '__main__':
@@ -99,10 +138,10 @@ if __name__ == '__main__':
                                             search='random', 
                                             source='xla', 
                                             )
-    train_dataloader = DataLoader(train_dataset, collate_fn=LayoutCollator(num_configs=64), num_workers=NUM_CPUS, batch_size=1, shuffle=True)
-    valid_dataloader = DataLoader(valid_dataset, collate_fn=LayoutCollator(num_configs=64), num_workers=NUM_CPUS, batch_size=1)
+    train_dataloader = DataLoader(train_dataset, collate_fn=LayoutCollator(num_configs=NUM_CONFIGS), num_workers=NUM_CPUS, batch_size=1, shuffle=True)
+    valid_dataloader = DataLoader(valid_dataset, collate_fn=LayoutCollator(num_configs=NUM_CONFIGS), num_workers=NUM_CPUS, batch_size=1)
 
-    model = GAT(args.hidden_channels, args.out_channels, args.heads).to(device)
+    model = GAT(args.hidden_channels, args.out_channels, args.heads, num_configs=NUM_CONFIGS).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=5e-4)
 
     print(model)
@@ -112,17 +151,25 @@ if __name__ == '__main__':
     for epoch in range(1, args.epochs + 1):
         
         for batch in train_dataloader:
-
             batch = batch.to(device)
-
+            
             start = time.time()
-            loss = train(batch, model, optimizer,)
-    #         train_acc, val_acc, tmp_test_acc = test()
-    #         if val_acc > best_val_acc:
-    #             best_val_acc = val_acc
-    #             test_acc = tmp_test_acc
-    #         log(Epoch=epoch, Loss=loss, Train=train_acc, Val=val_acc, Test=test_acc)
-    #         times.append(time.time() - start)
+            train_loss = train(batch, model, optimizer,)
+            log(Epoch=epoch, TrainLoss=train_loss,)
+            times.append(time.time() - start)
 
+        assert(0)
+        
+        for batch in valid_dataloader:
+            batch = batch.to(device)
+            
+            start = time.time()
+            val_acc, val_loss = validation(batch=batch, model=model,)
+            if val_acc > best_val_acc:
+                best_val_acc = val_acc
+            log(Epoch=epoch, ValLoss=val_loss, ValAcc=val_acc,)
+            times.append(time.time() - start)
+        
+        model.kendall_tau.reset()
 
-    # print(f"Median time per epoch: {torch.tensor(times).median():.4f}s")
+    print(f"Median time per epoch: {torch.tensor(times).median():.4f}s")
